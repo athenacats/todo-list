798b1169c160ddbf9e66e411b0d133ea8bbd23f2
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-01 19:03:11 +0300

    Add div today and change font color

--- src/modules/sidepanel.js
+++ src/modules/sidepanel.js
@@ -15,4 +15,15 @@ export default function sidePanelFunction() {
   text1.textContent = "Inbox";
   inbox.appendChild(text1);
   topPanel.appendChild(inbox);
+
+  const today = document.createElement("div");
+  today.setAttribute("id", "today");
+  const icon2 = document.createElement("div");
+  icon2.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>';
+  today.appendChild(icon2);
+  const text2 = document.createElement("h3");
+  text2.textContent = "Today";
+  today.appendChild(text2);
+  topPanel.appendChild(today);
 }
