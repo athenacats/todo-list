9ead833cc02ced89e0d0106a229b621a717a1db4
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-01 18:46:52 +0300

    Add inbox div to sidepanel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import startPage from "./modules/startpage";
 import todoitems from "./modules/factoryfunctions";
 import mainContainer from "./modules/container";
+import sidePanel from "./modules/sidepanel";
 
 import "./style.css";
 
 startPage();
 mainContainer();
+sidePanel();
 todoitems();
---
+++ src/modules/sidepanel.js
@@ -0,0 +1,18 @@
+export default function sidePanel() {
+  const sidePanel = document.querySelector(".sidePanel");
+
+  const topPanel = document.createElement("div");
+  topPanel.classList.add("topPanel");
+  sidePanel.appendChild(topPanel);
+
+  const inbox = document.createElement("div");
+  inbox.setAttribute("id", "inbox");
+  const icon1 = document.createElement("div");
+  icon1.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-inbox"><polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline><path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path></svg>';
+  inbox.appendChild(icon1);
+  const text1 = document.createElement("h3");
+  text1.textContent = "Inbox";
+  inbox.appendChild(text1);
+  topPanel.appendChild(inbox);
+}
