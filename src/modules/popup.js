0159306f7220e02fe630b23dea503fd59c87bc3e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-02 09:58:53 +0300

    Add few form details to popup for test

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -2,4 +2,22 @@ export default function taskPopUp() {
   const container = document.querySelector(".container");
   container.style.filter = "blur(20px)";
   container.style.webkitFilter = "blur(20px)";
+
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+  popup.style.filter = "none";
+  container.appendChild(popup);
+
+  const popupForm = document.createElement("form");
+  popup.setAttribute("id", "popupForm");
+  popup.appendChild(popupForm);
+
+  const popupTitle = document.createElement("input");
+  popupTitle.setAttribute("type", "text");
+  popupTitle.setAttribute("id", "popupTitle");
+  popupTitle.setAttribute("placeholder", "Title");
+  popupTitle.setAttribute("required");
+  popupForm.appendChild(popupTitle);
+
+  const popupContent = document.createElement("textarea");
 }
