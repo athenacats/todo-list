fee107fb848393e2666c8618892a428aee7c33e9
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-02 08:48:38 +0300

    Change sidepanel items to buttons

--- src/modules/sidepanel.js
+++ src/modules/sidepanel.js
@@ -5,8 +5,9 @@ export default function sidePanelFunction() {
   topPanel.classList.add("topPanel");
   sidePanel.appendChild(topPanel);
 
-  const inbox = document.createElement("div");
+  const inbox = document.createElement("button");
   inbox.setAttribute("id", "inbox");
+  inbox.setAttribute("type", "button");
   const icon1 = document.createElement("div");
   icon1.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-inbox"><polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline><path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path></svg>';
@@ -16,8 +17,9 @@ export default function sidePanelFunction() {
   inbox.appendChild(text1);
   topPanel.appendChild(inbox);
 
-  const today = document.createElement("div");
+  const today = document.createElement("button");
   today.setAttribute("id", "today");
+  today.setAttribute("type", "button");
   const icon2 = document.createElement("div");
   icon2.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>';
@@ -27,8 +29,9 @@ export default function sidePanelFunction() {
   today.appendChild(text2);
   topPanel.appendChild(today);
 
-  const upcoming = document.createElement("div");
-  today.setAttribute("id", "upcoming");
+  const upcoming = document.createElement("button");
+  upcoming.setAttribute("id", "upcoming");
+  upcoming.setAttribute("type", "button");
   const icon3 = document.createElement("div");
   icon3.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>';
@@ -51,6 +54,7 @@ export default function sidePanelFunction() {
 
   const addButton = document.createElement("button");
   addButton.setAttribute("id", "addButton");
+  addButton.setAttribute("type", "button");
   addButton.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
   sidePanel.appendChild(addButton);
