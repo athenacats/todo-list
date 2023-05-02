d9250dba720d3ad6aafc04c242ea01350c308e15
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-02 09:43:39 +0300

    Add popup js page and initial blur function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import startPage from "./modules/startpage";
 import todoitems from "./modules/factoryfunctions";
 import mainContainer from "./modules/container";
 import sidePanelFunction from "./modules/sidepanel";
+import taskPopUp from "./modules/popup";
 
 import "./style.css";
 
@@ -9,3 +10,11 @@ startPage();
 mainContainer();
 sidePanelFunction();
 todoitems();
+
+function buttonPopup() {
+  const addTaskButton = document.querySelector("#addButton");
+  addTaskButton.addEventListener("click", () => {
+    taskPopUp();
+  });
+}
+buttonPopup();
---
+++ src/modules/popup.js
@@ -0,0 +1,5 @@
+export default function taskPopUp() {
+  const container = document.querySelector(".container");
+  container.style.filter = "blur(20px)";
+  container.style.webkitFilter = "blur(20px)";
+}
