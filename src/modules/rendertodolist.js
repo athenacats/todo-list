a06f884665a6b8f90733de238612c7dcfb61dc84
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-09 18:38:14 +0300

    Add more DOM elements to rendertodolist.js

--- src/modules/rendertodolist.js
+++ src/modules/rendertodolist.js
@@ -11,4 +11,17 @@ export default function renderTodoList() {
   const todoListTitle = document.createElement("div");
   todoListTitle.classList.add("todoListTitle");
   todoListItem.appendChild(todoListTitle);
+
+  const todoListButton = document.createElement("button");
+  todoListButton.setAttribute("id", "todoListButton");
+  todoListButton.setAttribute("type", "button");
+  todoListItem.appendChild(todoListButton);
+
+  const todoListEdit = document.createElement("svg");
+  todoListEdit.classList.add("todoListEdit");
+  todoListItem.appendChild(todoListEdit);
+
+  const todoListDelete = document.createElement("svg");
+  todoListDelete.classList.add("todoListDelete");
+  todoListItem.appendChild(todoListDelete);
 }
