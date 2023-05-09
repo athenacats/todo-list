df799f31f41daa86e56a0072c5ca7f6f646aa27d
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-09 18:13:17 +0300

    Add some some dom elements for rendered todo list

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import startPage from "./modules/startpage";
 import mainContainer from "./modules/container";
 import sidePanelFunction from "./modules/sidepanel";
 import taskPopUp from "./modules/popup";
+import renderTodoList from "./modules/rendertodolist";
 
 import "./style.css";
 
 startPage();
 mainContainer();
 sidePanelFunction();
+renderTodoList();
 
 function buttonPopup() {
   const addTaskButton = document.querySelector("#addButton");
--- src/modules/rendertodolist.js
+++ src/modules/rendertodolist.js
@@ -1,3 +1,14 @@
-/* export default function renderTodoList() {
-  const content = document.querySelector(".content");
-} */
+export default function renderTodoList() {
+  const mainPanel = document.querySelector(".mainPanel");
+  const todoListItem = document.createElement("div");
+  todoListItem.classList.add("todoListItem");
+  mainPanel.appendChild(todoListItem);
+
+  const todoListPriority = document.createElement("svg");
+  todoListPriority.classList.add("todoListPriority");
+  todoListItem.appendChild(todoListPriority);
+
+  const todoListTitle = document.createElement("div");
+  todoListTitle.classList.add("todoListTitle");
+  todoListItem.appendChild(todoListTitle);
+}
