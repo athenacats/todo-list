999196b28eeeea784c9692372e5abce98fd4a251
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-11 14:07:38 +0300

    Clear form on submit

--- src/modules/formActions.js
+++ src/modules/formActions.js
@@ -9,6 +9,11 @@ export default function formActions() {
       buttonFunctions();
       const content = document.querySelector(".content");
       const popup = document.querySelector(".popup");
+      document.getElementById("popupTitle").value = "";
+      document.getElementById("popupDescription").value = "";
+      document.getElementById("duedate").value = "";
+      document.getElementById("duetime").value = "";
+      document.getElementById("priority").value = "";
       content.style.filter = "none";
       content.style.webkitFilter = "none";
       popup.style.display = "none";
