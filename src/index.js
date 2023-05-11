02544782966b71a0a4217a60ce9437af2d840f9d
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-11 11:32:48 +0300

    Move add todo task from index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import startPage from "./modules/startpage";
-
 import mainContainer from "./modules/container";
 import sidePanelFunction from "./modules/sidepanel";
-import taskPopUp from "./modules/popup";
 
 import "./style.css";
 
 startPage();
 mainContainer();
 sidePanelFunction();
-
-function buttonPopup() {
-  const addTaskButton = document.querySelector("#addButton");
-  addTaskButton.addEventListener("click", () => {
-    taskPopUp();
-  });
-}
-buttonPopup();
--- src/modules/sidepanel.js
+++ src/modules/sidepanel.js
@@ -1,3 +1,5 @@
+import taskPopUp from "./popup";
+
 export default function sidePanelFunction() {
   const sidePanel = document.querySelector(".sidePanel");
 
@@ -46,6 +48,7 @@ export default function sidePanelFunction() {
   addButton.setAttribute("type", "button");
   addButton.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
+  addButton.addEventListener("click", taskPopUp);
   sidePanel.appendChild(addButton);
 
   const tooltip1 = document.createElement("span");
