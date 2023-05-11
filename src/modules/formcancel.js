99ef5c38cbe239bc10325f8811fd6292db2b67d7
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-11 14:08:25 +0300

    Clear form on cancel

--- src/modules/formcancel.js
+++ src/modules/formcancel.js
@@ -1,6 +1,11 @@
 export default function cancelForm() {
   const content = document.querySelector(".content");
   const popup = document.querySelector(".popup");
+  document.getElementById("popupTitle").value = "";
+  document.getElementById("popupDescription").value = "";
+  document.getElementById("duedate").value = "";
+  document.getElementById("duetime").value = "";
+  document.getElementById("priority").value = "";
   content.style.filter = "none";
   content.style.webkitFilter = "none";
   popup.style.display = "none";
