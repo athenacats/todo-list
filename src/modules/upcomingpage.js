0193227a9adf1e3a2dfd95684d88101bf418c150
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-12 12:54:53 +0300

    Test: Upcoming succesfully works

--- src/modules/upcomingpage.js
+++ src/modules/upcomingpage.js
@@ -4,9 +4,7 @@ import { format, addDays, parseISO } from "date-fns";
 export default function upcomingPage() {
   const today = new Date();
   const dateFormatted = format(today, "yyyy-MM-dd");
-  console.log(dateFormatted);
   const formattedDate = parseISO(dateFormatted);
-  console.log(formattedDate);
   const week = [];
   const day1 = format(addDays(formattedDate, 1), "yyyy-MM-dd");
   const day2 = format(addDays(formattedDate, 2), "yyyy-MM-dd");
@@ -15,7 +13,6 @@ export default function upcomingPage() {
   const day5 = format(addDays(formattedDate, 5), "yyyy-MM-dd");
   const day6 = format(addDays(formattedDate, 6), "yyyy-MM-dd");
   week.push(dateFormatted, day1, day2, day3, day4, day5, day6);
-  console.log(week);
 
   const todos = document.querySelectorAll(".todoListDueDate");
 
