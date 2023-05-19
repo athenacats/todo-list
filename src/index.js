96a09b3d267aef60d6626d43bc536848372d0a36
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-19 16:41:21 +0300

    Set up dark mode

--- src/index.js
+++ src/index.js
@@ -41,17 +41,35 @@ function getFromLocalStorage() {
 // initially get everything from localStorage
 getFromLocalStorage();
 
-/* const today = new Date();
-const dateFormatted = format(today, "yyyy-MM-dd");
-console.log(dateFormatted);
-const formattedDate = parseISO(dateFormatted);
-console.log(formattedDate);
-const week = [];
-const day1 = format(addDays(formattedDate, 1), "yyyy-MM-dd");
-const day2 = format(addDays(formattedDate, 2), "yyyy-MM-dd");
-const day3 = format(addDays(formattedDate, 3), "yyyy-MM-dd");
-const day4 = format(addDays(formattedDate, 4), "yyyy-MM-dd");
-const day5 = format(addDays(formattedDate, 5), "yyyy-MM-dd");
-const day6 = format(addDays(formattedDate, 6), "yyyy-MM-dd");
-week.push(dateFormatted, day1, day2, day3, day4, day5, day6);
-console.log(week); */
+// set local storage for page on load
+let lightMode = localStorage.getItem("lightMode"); // name it whatever you want
+const lightModeToggle = document.querySelector("#lightModeToggle"); // accessed from button on page. on hindsight could have used class on svg
+
+// this function is to enable light mode
+const enableLightMode = () => {
+  document.body.classList.add("lightTheme");
+  localStorage.setItem("lightMode", "enabled"); // the two items in the brackets are keys. i could have named the second one anything like yes, active, etc
+};
+
+// this disables lightmode
+const disableLightMode = () => {
+  document.body.classList.remove("lightTheme");
+  localStorage.setItem("lightMode", null); // same here. second one could have been a string
+};
+
+// checks if lightmode is enabled once a page loads
+if (lightMode === "enabled") {
+  enableLightMode();
+} else {
+  disableLightMode();
+}
+
+// toggles between the two themes
+lightModeToggle.addEventListener("click", () => {
+  lightMode = localStorage.getItem("lightMode");
+  if (lightMode !== "enabled") {
+    enableLightMode();
+  } else {
+    disableLightMode();
+  }
+});
