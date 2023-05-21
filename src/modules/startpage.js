79c08cc9de0db3d7504ceaf66651d6f85f452bce
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-21 13:25:48 +0300

    Add mobile css

--- src/modules/startpage.js
+++ src/modules/startpage.js
@@ -9,6 +9,13 @@ export default function startPage() {
   header.classList.add("header");
   container.appendChild(header);
 
+  const menu = document.createElement("button");
+  menu.classList.add("menu");
+  menu.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>';
+  menu.style.display = "none";
+  header.appendChild(menu);
+
   const title = document.createElement("h1");
   title.textContent = "To-Do List";
   header.appendChild(title);
